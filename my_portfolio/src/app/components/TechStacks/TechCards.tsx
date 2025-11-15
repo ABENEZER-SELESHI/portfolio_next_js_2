7360463aa29c42408b9f49956da12a9483c6ee69
ABENEZER-SELESHI <ebenezerseleshi@gmail.com>
2025-11-15 13:16:25 +0300

    fix props

--- my_portfolio/src/app/components/TechStacks/TechCards.tsx
+++ my_portfolio/src/app/components/TechStacks/TechCards.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { IconType } from "react-icons";
 
-const TechCards = ({ icon: Icon, name }) => {
+interface TechCardProps {
+  icon: IconType;
+  name: string;
+}
+
+const TechCards: React.FC<TechCardProps> = ({ icon: Icon, name }) => {
   return (
     <div className="bg-[var(--color-bg-primary)] text-[var(--color-text-primary)] rounded p-3 flex flex-col items-center justify-center gap-4 shadow-md hover:scale-105 transition-transform">
       <Icon className="text-4xl" />
